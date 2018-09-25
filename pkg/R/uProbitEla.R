798ed1a9264c7dfd3ae5bfbc0c5a10ec7d73a536
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-09-25 15:09:40 +0000

    uProbitEla() now uses uProbitElaDeriv() to calculated the partial derivatives of the semi-elasticity wrt to the coefficients
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/uProbitEla.R
+++ pkg/R/uProbitEla.R
@@ -10,14 +10,12 @@ uProbitEla <- function( allCoef, allXVal,
   checkXPos( xPos, minLength = 1, maxLength = 2, minVal = 1, maxVal = nCoef )
   if( length( xPos ) == 2 ){
     xCoef <- allCoef[ xPos ]
-    xCoefSE <- allCoefSE[ xPos ]
     if( !isTRUE( all.equal( allXVal[ xPos[2] ], allXVal[ xPos[1] ]^2 ) ) ) {
       stop( "the value of 'allXVal[ xPos[2] ]' must be equal",
         "to the squared value of 'allXVal[ xPos[1] ]' " )
     }
   } else if( length( xPos ) == 1 ) {
     xCoef <- c( allCoef[ xPos ], 0 )
-    xCoefSE <- c( allCoefSE[ xPos ], 0 )
   } else {
     stop( "argument 'xPos' must be a scalar or a vector with two elements" )
   }
@@ -26,9 +24,8 @@ uProbitEla <- function( allCoef, allXVal,
   checkXBeta( xBeta )
   dfun <- dnorm( xBeta )
   semEla <- ( xCoef[ 1 ] + 2 * xCoef[ 2 ] * xVal ) * xVal * dfun
-  derivCoef <- c( dfun * xVal, 
-    ifelse( length( xPos ) == 1, 0, dfun * 2 * xVal^2 ) )
-  vcovCoef <- diag( xCoefSE^2 )
+  derivCoef <- uProbitElaDeriv( allCoef, allXVal, xPos, simplified = TRUE )
+  vcovCoef <- diag( allCoefSE^2 )
   semElaSE <- drop( sqrt( t( derivCoef ) %*% vcovCoef %*% derivCoef ) )
   result <- c( semEla = semEla, stdEr = semElaSE )
   return( result )
