6e1b40a589bcd2c4bcb329c0cddd5a5ee686a803
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-09-25 13:59:23 +0000

    added test script for analyses based on results from probit models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/probit.R
@@ -0,0 +1,45 @@
+library( "urbin" )
+data( "Mroz87", package = "sampleSelection" )
+
+# create dummy variable for kids
+Mroz87$kids <- as.numeric( Mroz87$kids5 > 0 | Mroz87$kids618 > 0 )
+
+### linear in age
+estProbitLin <- glm( lfp ~ kids + age + educ, 
+  family = binomial(link = "probit"), 
+  data = Mroz87 )
+summary( estProbitLin )
+# mean values of the explanatory variables
+xMeanLin <- c( 1, colMeans( Mroz87[ , c( "kids", "age", "educ" ) ] ) )
+# semi-elasticity of age without standard errors
+uProbitEla( coef( estProbitLin ), xMeanLin, xPos = 3 )
+# semi-elasticity of age based on numerical derivation
+100 * ( predict( estProbitLin, 
+  newdata = as.data.frame( t( xMeanLin * c( 1, 1, 1.005, 1 ) ) ), 
+  type = "response" ) -
+    predict( estProbitLin, 
+      newdata = as.data.frame( t( xMeanLin * c( 1, 1, 0.995, 1 ) ) ), 
+      type = "response" ) )
+# semi-elasticity of age with standard errors
+uProbitEla( coef( estProbitLin ), xMeanLin, 
+  sqrt( diag( vcov( estProbitLin ) ) ), 3 )
+
+### quadratic in age
+estProbitQuad <- glm( lfp ~ kids + age + I(age^2) + educ, 
+  family = binomial(link = "probit"), 
+  data = Mroz87 )
+summary( estProbitQuad )
+# mean values of the explanatory variables
+xMeanQuad <- c( xMeanLin[ 1:3], xMeanLin[3]^2, xMeanLin[4] )
+# semi-elasticity of age without standard errors
+uProbitEla( coef( estProbitQuad ), xMeanQuad, xPos = c( 3, 4 ) )
+# semi-elasticity of age based on numerical derivation
+100 * ( predict( estProbitQuad, 
+  newdata = as.data.frame( t( xMeanQuad * c( 1, 1, 1.005, 1.005^2, 1 ) ) ), 
+  type = "response" ) -
+    predict( estProbitQuad, 
+      newdata = as.data.frame( t( xMeanQuad * c( 1, 1, 0.995, 0.995^2, 1 ) ) ), 
+      type = "response" ) )
+# semi-elasticity of age with standard errors
+uProbitEla( coef( estProbitQuad ), xMeanQuad, 
+  sqrt( diag( vcov( estProbitQuad ) ) ), c( 3, 4 ) )
