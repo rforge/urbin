56851f4ee7a56f1bc54d6d21a29fcc0c6148235d
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-09-27 17:02:41 +0000

    added code to test uProbitElaIntDeriv()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/probit.R
+++ pkg/tests/probit.R
@@ -137,6 +137,12 @@ xMeanInt <- c( xMeanLin[1:2], mean( Mroz87$age30.37 ),
 # semi-elasticity of age without standard errors
 uProbitElaInt( coef( estProbitInt ), xMeanInt, 
   c( 3, 4, 0, 5 ), c( 30, 37.5, 44.5, 52.5, 60 ) )
+# partial derivatives of the semi-elasticity wrt the coefficients
+urbin:::uProbitElaIntDeriv( coef( estProbitInt ), xMeanInt, 
+  c( 3, 4, 0, 5 ), c( 30, 37.5, 44.5, 52.5, 60 ) )
+# numerically computed partial derivatives of the semi-elasticity wrt the coefficients
+numericGradient( uProbitElaInt, t0 = coef( estProbitInt ), allXVal = xMeanInt, 
+  xPos = c( 3, 4, 0, 5 ), xBound = c( 30, 37.5, 44.5, 52.5, 60 ) )
 # semi-elasticity of age with standard errors (full covariance matrix)
 uProbitElaInt( coef( estProbitInt ), xMeanInt, 
   c( 3, 4, 0, 5 ), c( 30, 37.5, 44.5, 52.5, 60 ), 
