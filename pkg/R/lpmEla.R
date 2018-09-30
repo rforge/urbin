7fd559fe248d13a7551b73db277bead9a1bb29e4
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-09-30 10:17:42 +0000

    lpmEla(): renamed argument xCoefSE as xCoefVcov, which can now also be the variance covariance matrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/lpmEla.R
+++ pkg/R/lpmEla.R
@@ -1,21 +1,23 @@
-lpmEla <- function( xCoef, xVal, xCoefSE = rep( NA, length( xCoef ) ) ){
-  if( ! length( xCoef ) %in% c( 1, 2 ) ) {
+lpmEla <- function( xCoef, xVal, xCoefVcov = NULL ){
+  # number of coefficients
+  nCoef<- length( xCoef )
+  if( ! nCoef %in% c( 1, 2 ) ) {
     stop( "argument 'xCoef' must be a scalar or vector with 2 elements" )
   }
-  if( length( xCoef ) != length( xCoefSE ) ) {
-    stop( "arguments 'xCoef' and 'xCoefSE' must have the same length" )
-  }
+  # check and prepare allCoefVcov
+  xCoefVcov <- prepareVcov( xCoefVcov, nCoef, xPos = 1:nCoef, xMeanSd = NULL )
+  # check argument xVal
   if( length( xVal ) != 1 || !is.numeric( xVal ) ) {
     stop( "argument 'xVal' must be a single numeric value" )
   }
-  if( length( xCoef ) == 1 ) {
+  if( nCoef == 1 ) {
     xCoef <- c( xCoef, 0 )
-    xCoefSE <- c( xCoefSE, 0 )
+    xCoefVcov <- matrix( c( xCoefVcov, 0, 0, 0 ), nrow = 2, ncol = 2 )
   }
   semEla <- ( xCoef[1] + 2 * xCoef[2] * xVal ) * xVal
   derivCoef <- c( xVal, ifelse( xCoef[2] == 0, 0, 2 * xVal^2 ) )
-  vcovCoef <- diag( xCoefSE^2 )
-  semElaSE <- drop( sqrt( t( derivCoef ) %*% vcovCoef %*% derivCoef ) )
+  # approximate standard error of the semi-elasticity
+  semElaSE <- drop( sqrt( t( derivCoef ) %*% xCoefVcov %*% derivCoef ) )
   result <- c( semEla = semEla, stdEr = semElaSE )
   return( result )
 }
