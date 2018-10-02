0e0842bfc4a712ccf7c2ee3d8a6c8bad1c178a08
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-02 19:56:40 +0000

    lpmEffInt(): renamed argument allCoefSE as allCoefVcov, necause this argument can now also be the variance covariance matrix; now using prepareVcov() to check and prepare the variance covariance matrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@141 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/lpmEffInt.R
+++ pkg/R/lpmEffInt.R
@@ -1,16 +1,15 @@
 lpmEffInt <- function( allCoef, refBound, intBound, xPos,
-  allCoefSE = rep( NA, length( allCoef ) ) ){
+  allCoefVcov = NULL ){
 
   # number of coefficients
   nCoef <- length( allCoef )
   # Check position vector
   checkXPos( xPos, minLength = 1, maxLength = 2, minVal = 1, maxVal = nCoef )
+  # check and prepare allCoefVcov
+  allCoefVcov <- prepareVcov( allCoefVcov, nCoef, xPos, xMeanSd = NULL )
   # check the boundaries of the intervals
   refBound <- elaIntBounds( refBound, 1, argName = "refBound" )
   intBound <- elaIntBounds( intBound, 1, argName = "intBound" )
-  if( length( allCoef ) != length( allCoefSE ) ) {
-    stop( "arguments 'allCoef' and 'allCoefSE' must have the same length" )
-  }
   xCoef <- allCoef[xPos]
   if( length( xCoef ) == 1 ) {
     xCoef <- c( xCoef, 0 )
@@ -29,14 +28,8 @@ lpmEffInt <- function( allCoef, refBound, intBound, xPos,
   if( length( xPos ) == 2 ) {
     derivCoef[ xPos[2] ] <- xSquaredDiff
   }
-  # variance covariance of the coefficients (covariances set to zero)
-  if( length( allCoefSE ) > 1 ) {
-    vcovCoef <- diag( allCoefSE^2 )
-  } else {
-    vcovCoef <- allCoefSE^2
-  }
   # approximate standard error of the effect
-  effSE <- drop( sqrt( t( derivCoef ) %*% vcovCoef %*% derivCoef ) )
+  effSE <- drop( sqrt( t( derivCoef ) %*% allCoefVcov %*% derivCoef ) )
   # object to be returned
   result <- c( effect = eff, stdEr = effSE )
   return( result )
