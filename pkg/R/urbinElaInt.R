a62c653c25d2c2da179f4469d1e788a260c033cc
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-03 13:50:34 +0000

    moved code in fuction calcSharesInt() (back?) to urbinElaInt(), because this code/function won't be used in any other part of the package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@182 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/urbinElaInt.R
+++ pkg/R/urbinElaInt.R
@@ -17,7 +17,25 @@ urbinElaInt <- function( allCoef, allXVal, xPos, xBound, model,
   # check and prepare allCoefVcov
   allCoefVcov <- prepareVcov( allCoefVcov, nCoef, xPos, xMeanSd = NULL )
   # vector of shares of observations in each interval
-  shareVec <- calcSharesInt( allXVal, xPos )
+  shareVec <- rep( NA, nInt )
+  for( i in 1:nInt ){
+    if( xPos[i] != 0 ) {
+      shareVec[ i ] <- allXVal[ xPos[ i ] ] 
+    }
+  }
+  if( any( shareVec[ xPos != 0 ] < 0 ) ) {
+    stop( "all elements of argument 'allXVal'",
+      " that are indicated by argument 'xPos'",
+      " (i.e., the shares of observations in each interval)",
+      " must be non-negative" )
+  }
+  if( sum( shareVec[ xPos != 0 ] ) > 1 + sqrt(.Machine$double.eps) ) {
+    stop( "the sum of the elements of argument 'allXVal'",
+      " that are indicated by argument 'xPos'",
+      " (i.e., the shares of observations in each interval)",
+      " must not be larger than one" )
+  }
+  shareVec[ xPos == 0 ] <- 1 - sum( shareVec[ xPos != 0 ] )
   # weights
   weights <- elaIntWeights( shareVec )
   # prepare calculation of semi-elasticity 
