158beebee5ea3f713c9f10cde32d4d99976c608b
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-04 14:57:20 +0000

    added test script logitEffCat.R (copied from the on-line appendix); added the resulting output file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@226 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/logitEffCat.R
@@ -0,0 +1,45 @@
+library( "urbin" )
+
+# Example
+eff10a <- urbin:::logitEffCat( allCoef = c( 0.28, 0.003, 0.0075, 0, -0.034, 
+  -0.005, 0.89, -1.2 ), 
+  allXVal = c( 1, 0.1, 0.3, 0.25, 0.15, 0.2, 2.34, 10.8 ), 
+  xPos = c( 2:6 ), Group = c( 0, -1, -1, 1, 1 ) )
+eff10a
+
+eff10b <- urbin:::logitEffCat( allCoef = c( 0.28, 0.003, 0.0075, 0, -0.034, 
+  -0.005, 0.89, -1.2 ), 
+  allXVal = c( 1, 0.1, 0.3, 0.25, 0.15, 0.2, 2.34, 10.8 ), 
+  xPos = c( 2:6 ), Group = c( 0, -1, -1, 1, 1 ), 
+  allCoefSE = c( 0.03, 0.0001, 0.005, 0, 0.01, 
+    0.004, 0.05, 0.8 ) )
+eff10b
+
+# Example
+eff11a <- urbin:::logitEffCat( allCoef = c( 0.28, 0.003, 0.0075, 0, -0.034, 
+  -0.005, 0.89, 0, 0.005, 0.06, 1.7, 0 ),
+  allXVal = c( 1, 0.5, 0.3, 0.2 ), xPos = c( 2:4 ), 
+  Group = c( -1, -1, 1 ), yCat = 2, method = "MNL" )
+eff11a
+
+eff11b <- urbin:::logitEffCat( allCoef = c( 0.28, 0.003, 0.0075, 0, -0.034, 
+  -0.005, 0.89, 0, 0.005, 0.06, 1.7, 0 ),
+  allXVal = c( 1, 0.5, 0.3, 0.2 ), xPos = c( 2:4 ), 
+  Group = c( -1, -1, 1 ), yCat = 2, method = "MNL", 
+  allCoefSE = c( 0.03, 0.0001, 0.005, 0, 0.01, 0.004, 
+    0.05, 0, 0.004, 0.5, 0.0078, 0 ) )
+eff11b
+
+# Example
+eff12a <- urbin:::logitEffCat( allCoef = c( 0.28, 0.003, 0.0075, 0 ),
+  allXVal = c( 1, 0.5, 0.3, 0.2, 1, 0.4, 0.4, 0.1 ), 
+  xPos = c( 2:4 ), 
+  Group = c( -1, -1, 1 ), yCat = 2, method = "CondL" )
+eff12a
+
+eff12b <- urbin:::logitEffCat( allCoef = c( 0.28, 0.003, 0.0075, 0 ),
+  allXVal = c( 1, 0.5, 0.3, 0.2, 1, 0.4, 0.4, 0.1 ), 
+  xPos = c( 2:4 ), 
+  allCoefSE = c( 0.03, 0.0001, 0.005, 0 ),
+  Group = c( -1, -1, 1 ), yCat = 2, method = "CondL" )
+eff12b
