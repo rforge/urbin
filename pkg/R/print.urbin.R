a2bcb580b47703816d55d0f8bedce97e030cb882
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-11 15:23:39 +0000

    changed print.urbin() so that further components can be added to the returned object without changing the output of print.urbin()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@308 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/print.urbin.R
+++ pkg/R/print.urbin.R
@@ -1,4 +1,12 @@
 print.urbin <- function( x, ... ) {
+  if( "semEla" %in% names( x ) ) {
+    printVec <- c( x$semEla, x$stdEr )
+  } else if( "effect" %in% names( x ) ) {
+    printVec <- c( x$effect, x$stdEr )
+  } else {
+    stop( "internal error: object of class 'urbin' does not include",
+      " expected components" )
+  }
   print( unlist( x ) )
   invisible( x )
 }
