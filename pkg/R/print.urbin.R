8a5cf16ee2bb751c6b634852c75f2dc508ffedb6
arne <arne@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-11 15:32:04 +0000

    fixed 2 mistakes that I made in the previous revision (now indeed further components can be added to the returned object without changing the output of print.urbin())
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/urbin@309 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/print.urbin.R
+++ pkg/R/print.urbin.R
@@ -1,12 +1,12 @@
 print.urbin <- function( x, ... ) {
   if( "semEla" %in% names( x ) ) {
-    printVec <- c( x$semEla, x$stdEr )
+    printVec <- c( semEla = x$semEla, stdEr = x$stdEr )
   } else if( "effect" %in% names( x ) ) {
-    printVec <- c( x$effect, x$stdEr )
+    printVec <- c( effect = x$effect, stdEr = x$stdEr )
   } else {
     stop( "internal error: object of class 'urbin' does not include",
       " expected components" )
   }
-  print( unlist( x ) )
+  print( printVec )
   invisible( x )
 }
